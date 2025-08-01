2a90e7c0156e08ce199b8299d0eca9e33de267c8
julianmeyerr <j.meyer@alumno.etec.um.edu.ar>
2025-08-01 12:16:59 -0300

    resolucion ejercicio 6

--- 06-herramientas/src/ejercicio.js
+++ 06-herramientas/src/ejercicio.js
@@ -1,8 +1,10 @@
 // EJERCICIO: Instala la dependencia dayjs con npm y úsala para mostrar la fecha, con TODOs y comentarios guía.
 // import dayjs from 'dayjs';
-
+import dayjs from 'dayjs';
 // TODO: Usa dayjs para obtener la fecha y hora actual y mostrarla en el DOM
 // const now = ...
 // document.body.innerHTML = ...
+const fecha = dayjs().format('DD/MM/YYYY HH:mm:ss');
+document.body.innerHTML = `<h2>Fecha y hora actual: ${fecha}</h2>`;
 
-// Puedes ejecutar este archivo con Vite y ver el resultado en el navegador 
+// Puedes ejecutar este archivo con Vite y ver el resultado en el navegador
